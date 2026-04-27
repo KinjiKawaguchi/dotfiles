f80d8f091474cb71914d3e62984e22c39c66c341
KinjiKawaguchi <kawakin0310@icloud.com>
2026-04-27 17:30:27 +0900

    feat(tmux,nvim): SSH 越しに OSC 52 でクリップボード連携する
    リモート開発時にローカルクリップボードへ yank できるよう、
    tmux で OSC 52 を terminal に通過させ、nvim 側で SSH_TTY 検知時に
    osc52 clipboard provider を有効化する。pbcopy 直接呼び出しは廃止する。

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -11,3 +11,20 @@ vim.opt.spell = false
 -- Auto-reload files changed by external processes
 vim.o.autoread = true
 
+if os.getenv("SSH_TTY") then
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+  -- LazyVim は SSH 時に clipboard を空にするので、OSC 52 を使う場合は再度有効化
+  vim.opt.clipboard = "unnamedplus"
+end
+
